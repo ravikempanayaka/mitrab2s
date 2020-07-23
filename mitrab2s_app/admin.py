22000f2290366eaa47a12800c3fb48abf457dd96
kravi <ravi.k@agiliti.in>
2020-07-23 11:36:48 +0530

    django admin dashboard list dispay set in answer models

--- mitrab2s_app/admin.py
+++ mitrab2s_app/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from .models import Question, Answer
 
 # Register your models here.
-admin.site.register(Answer)
+# admin.site.register(Answer)
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ['user_id', 'question_text']
 
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['user_id', 'question_id', 'answer_text', 'upvote']
